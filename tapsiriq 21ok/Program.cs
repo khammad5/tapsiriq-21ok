9552c4aea7974a92724e586022ba26b0be74b8d0
khammad5 <khammadakh@code.edu.az>
2023-10-22 02:01:32 +0400

    Add project files.

---
+++ tapsiriq 21ok/Program.cs
@@ -0,0 +1,44 @@
+﻿#region task2
+
+//int skafeni = 115;
+//int skafuzun = 140;
+//int skafhun = 70;
+
+//int qapieni = 100;
+//int qapihun = 200;
+
+//if ((skafeni < qapieni && skafuzun == qapihun) || (skafeni < qapieni && skafhun == qapihun) || (skafuzun<qapieni&&skafeni<qapihun)||
+//    (skafuzun < qapieni && skafhun < qapihun) || (skafhun < qapieni && skafeni < qapihun) || (skafhun < qapieni && skafuzun < qapihun)) 
+//        {
+//    Console.WriteLine("qapi kecir");
+//}
+//else 
+//{
+//    Console.WriteLine("kecmir");
+//}
+#endregion
+
+#region task1
+
+int a = 60;
+//int b = 35;
+//int count = 0;
+//int result = a + b;
+//while (a > 0) 
+//{
+//    result /= 10;
+//    count++;
+//}
+//if (count == 2) 
+//{
+//    Console.WriteLine("ikireqemlidir");
+//}
+//else if (count == 3) 
+//{
+//    Console.WriteLine("uc reqemlidir");
+//}
+//else 
+//{
+//    Console.WriteLine("eded nezere alinmir");
+//}
+#endregion
